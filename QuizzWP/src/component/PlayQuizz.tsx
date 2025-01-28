1e7632da6bed6c3c0b51c36d97725003daf10ec7
Magaly <fernandezgarciamagaly@gmail.com>
2025-01-28 14:54:07 +0100

    redirecting when refresh quizz

--- QuizzWP/src/component/PlayQuizz.tsx
+++ QuizzWP/src/component/PlayQuizz.tsx
@@ -3,13 +3,21 @@ import { fetchQuestionsByID } from "../services/questionsService";
 import { QuestionsByID } from "../types/questions";
 import { QuizzList } from "../types/quizz";
 import { fetchQuizzByID } from "../services/quizzlistService";
+import { redirect, useNavigate } from "react-router-dom";
 
 const PlayQuizz = ({ quizzId }: { quizzId: number }) => {
   const [questionsList, setQuestionsList] = useState<QuestionsByID[]>([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
+  console.log("quizzId", quizzId);
+  let navigate = useNavigate();
+  
+
   useEffect(() => {
     let ignore = false;
+    if (quizzId === 0) {
+      navigate("/");
+    }
 
     fetchQuizzByID(quizzId).then((result) => {
       result.map((question) => {
@@ -28,15 +36,19 @@ const PlayQuizz = ({ quizzId }: { quizzId: number }) => {
     };
   }, [quizzId]);
 
+ 
+
   if (questionsList.length > 0) {
     return <section>{questionsList[currentQuestion].name} {
         
     }</section>
     
   }
+
+  
   
 
-  return <div>Quizz en construction</div>;
+  
 };
 
 export default PlayQuizz;
