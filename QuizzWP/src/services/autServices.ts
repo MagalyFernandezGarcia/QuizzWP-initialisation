074f178b5043cd44ea3359d0984d44427ebb6cb3
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2025-02-03 11:05:12 +0100

    fix the url on the auth service

--- QuizzWP/src/services/autServices.ts
+++ QuizzWP/src/services/autServices.ts
@@ -1,26 +1,22 @@
+export async function LoginUser(
+	username: string,
+	password: string
+): Promise<Boolean> {
+	const formData = new URLSearchParams();
+	formData.append("log", username);
+	formData.append("pwd", password);
 
-const {VITE_URL_WP} = import.meta.env
-export async function LoginUser(username: string, password: string) :Promise<Boolean>{
-    const formData = new URLSearchParams()
-    formData.append('log', username)
-    formData.append('pwd', password)
+	try {
+		const response = await fetch("http://quizz.local/wp-login.php", {
+			method: "POST",
+			headers: { "content-Type": "application/x-www-form-urlencoded" },
+			body: formData.toString(),
+			credentials: "include",
+		});
 
-    try{
-        const response = await fetch("http://" +VITE_URL_WP + 'wp-login.php', { 
-            method: 'POST', 
-            headers :{"content-Type" : "application/x-www-form-urlencoded"},
-            body : formData.toString(),
-            credentials: 'include'
-        })
-           
-        return response.ok
-    }catch(error){
-        console.error("Erreur lors de la connexion" , error)
-        throw error
-    }
-            
-      
-    
-    
-    
+		return response.ok;
+	} catch (error) {
+		console.error("Erreur lors de la connexion", error);
+		throw error;
+	}
 }
